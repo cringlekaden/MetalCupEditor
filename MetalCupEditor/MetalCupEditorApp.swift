ab24327a19d6e0847db8a658606697ea5ca96b8b
cringlekaden <cringlekaden@gmail.com>
2026-02-03 22:00:12 -0500

    Initial Commit

---
+++ MetalCupEditor/MetalCupEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalCupEditorApp.swift
+//  MetalCupEditor
+//
+//  Created by Kaden Cringle on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MetalCupEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
